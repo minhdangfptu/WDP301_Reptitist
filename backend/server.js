8373c0487ed23b5f963063f5b71ab592c9e1f54f
Minh Đặng <minhdangdinh261004@gmail.com>
2025-05-14 11:38:48 +0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { connect } = require('mongoose');
+const router = require('./src/routes/index.js');
+const dotenv = require('dotenv');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+dotenv.config();
+app.use(cors());
+
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+connect(MONGODB_URI);
+
+
+// For parsing application/json
+//app.use(express.json());
+app.use(bodyParser.json());
+app.use('/', router);
+
+router.get("/test", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
