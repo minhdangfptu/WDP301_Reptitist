581ffae6db76092249293a4941f7422a601a3da1
qhuy2412 <doquanghuy24122003@gmail.com>
2025-05-20 21:06:25 +0700

    Add backend authentication logic

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const User = require('../models/Users');
+const jwt = require('jsonwebtoken');
+const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.headers('Authorization')?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Access denied!No token provided.' });
+        }
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password_hashed -__v');
+        if (!req.user) {
+            return res.status(401).json({ message: 'Invalid token!' });
+        }
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+};
+module.exports = authMiddleware;
