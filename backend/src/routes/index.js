5bf334c768582677615d300b6379177bc60e168e
Tien03 <tienddhe176681@fpt.edu.vn>
2025-05-23 13:19:35 +0700

    view librarycategory and dont check admin

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,10 +6,16 @@ const router = express.Router();
 // const feedbackRoutes = require("./feedbackRoutes.js")
 // const transactionRoutes = require("./transactionRoutes.js")
 
-// router.use('/cars', carRoutes)
-// router.use("/users", userRoutes)
-// router.use("/bookings", bookingRoutes)
-// router.use("/feedbacks", feedbackRoutes)
-// router.use("/transactions", transactionRoutes)
+=======
+const authRoute=require('./authRoute.js')
+const shopRoute=require('./shopRoute.js')
+const reptileRoutes = require('./reptile');
+const libraryCategoriesRoute = require('./libraryCategoriesRoute');
+const router =express.Router();
 
+router.use('/auth',authRoute);
+router.use('/shop',shopRoute);
+router.use('/info', reptileRoutes);
+router.use('/library_categories', libraryCategoriesRoute);
+>>>>>>> Stashed changes
 module.exports= router;
