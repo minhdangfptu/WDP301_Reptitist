7736670e73c2219867a66a961fb84c7b53f0dfed
Tien03 <tienddhe176681@fpt.edu.vn>
2025-05-23 13:31:16 +0700

    view categoryLibrary and donot check admin

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const router = express.Router();
-// const carRoutes = require("./carRoutes.js")
-// const userRoutes = require("./userRoutes.js")
-// const bookingRoutes = require("./bookingRoutes.js")
-// const feedbackRoutes = require("./feedbackRoutes.js")
-// const transactionRoutes = require("./transactionRoutes.js")
+const express= require('express');
 
-=======
 const authRoute=require('./authRoute.js')
 const shopRoute=require('./shopRoute.js')
 const reptileRoutes = require('./reptile');
@@ -16,5 +9,5 @@ const router =express.Router();
 router.use('/auth',authRoute);
 router.use('/shop',shopRoute);
 router.use('/info', reptileRoutes);
-
+router.use('/library_categories', libraryCategoriesRoute);
 module.exports= router;
