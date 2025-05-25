722ae48096fc80761791d0f5f0c457731b92edff
Minh Đặng <minhdangdinh261004@gmail.com>
2025-05-25 13:38:37 +0700

    wtf

--- backend/src/models/Library_contents.js
+++ backend/src/models/Library_contents.js
@@ -6,7 +6,7 @@ const libraryContentSchema = new mongoose.Schema({
   image_urls: { type: [String], default: [] },
   created_at: { type: Date, default: Date.now },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  topicy_id: { type: mongoose.Schema.Types.ObjectId, ref: 'TopicCategory', required: true  },
+  topic_id: { type: mongoose.Schema.Types.ObjectId, ref: 'TopicCategory', required: true  },
   category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'LibraryCategories', required: true  }
 }, { collection: 'library_contents' });
 
