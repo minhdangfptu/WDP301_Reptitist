f657c33b4411aaa3f189a3dbcffe69e3c71b67cf
Tien03 <tienddhe176681@fpt.edu.vn>
2025-05-30 10:46:50 +0700

    fix libraryCategory

--- backend/src/models/Library_category.js
+++ backend/src/models/Library_category.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-  category_id: {
-    type: String,
-    required: true,
-    unique: true,
-    default: () => new mongoose.Types.ObjectId().toHexString()
-  },
+  
   category_content: { type: String, required: true },
   category_description: { type: String },
   category_imageurl: { type: String }
