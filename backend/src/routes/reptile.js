840e63886d54573804655b7f5e1f5245ba476e68
justduchuy15 <140197989+justduchuy15@users.noreply.github.com>
2025-06-07 02:19:28 +0700

    middleware cua huy

--- backend/src/routes/reptile.js
+++ backend/src/routes/reptile.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const reptileController = require('../controllers/reptileController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware1 = require('../middleware/authMiddlewareModify')
 const roleMiddleware = require('../middleware/roleMiddleware');
 
 //Api create repltile infomation: Chỉ admin mới có quyền thực hiện api này
 router.post(
   '/create-reptile',
-  authMiddleware,
-  roleMiddleware('admin'),
-  reptileController.createReptile
+  authMiddleware1,
+  roleMiddleware('admin'),         // Kiểm tra quyền admin
+  reptileController.createReptile // Hàm callback cho việc tạo reptile
 );
 
 //Api lấy ra tất cả reptile infomation: Tất cả mọi người đều có quyền thực hiện api này
@@ -22,21 +22,20 @@ router.get(
 //Api cập nhật reptile infomation: Chỉ admin mới có quyền thực hiện api này
 router.put(
   '/update-reptile',
-  authMiddleware,
-  roleMiddleware('admin'),
+authMiddleware1,
+roleMiddleware('admin'),  
   reptileController.updateReptileById
 );
 
 //Api xóa reptile infomation: Chỉ admin mới có quyền thực hiện api này
 router.delete(
   '/delete-reptile',
-  authMiddleware,
-  roleMiddleware('admin'),
+roleMiddleware('admin'),  
   reptileController.deleteReptileById
 );
 
 //Api lấy ra reptile infomation theo user đang đăng nhập
-router.get('/my-reptiles', authMiddleware, reptileController.getReptilesByUser);
+router.get('/my-reptiles', reptileController.getReptilesByUser);
 
 //Api lấy ra reptile infomation theo id: Tất cả mọi người đều có quyền thực hiện api này
 router.get(
