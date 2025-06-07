c84f9647074c8c64c6bddc40bb3e0d5ac28531e8
qhuy2412 <doquanghuy24122003@gmail.com>
2025-06-07 23:43:25 +0700

    chore: remove .env from tracking

--- backend/src/config/passport-google.js
+++ backend/src/config/passport-google.js
@@ -2,12 +2,10 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/users');
 const Role = require('../models/Roles');
-console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/reptitist/api/auth/google/callback'
+    callbackURL: 'http://localhost:8080/reptitist/auth/google/callback'
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
