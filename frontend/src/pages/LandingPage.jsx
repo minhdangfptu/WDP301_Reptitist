029a25c889d7b385ed524a3e4f2a7f863c08463f
Minh Đặng <minhdangdinh261004@gmail.com>
2025-06-15 23:28:15 +0700

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
 import ReactPlayer from "react-player";
 
 console.log("huhuhuhuhuhuhuhu")
