711e5b116349b6866155a0bf4eddb233c727f4c4
Minh Đặng <minhdangdinh261004@gmail.com>
2025-06-16 18:22:39 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ const corsOptions = {
     'http://localhost:3000', // React development server
     'http://localhost:5173', // Vite development server  
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    process.env.FRONTEND_URL1,
+    process.env.FRONTEND_URL2
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
